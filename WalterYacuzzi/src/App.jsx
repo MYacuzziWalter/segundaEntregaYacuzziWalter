e3c858750e1e1409a42bb05c36ef8b4d454ff68f
MYacuzziWalter <walteryacuzzi@gmail.com>
2024-10-14 17:39:08 -0300

    comentario en app

--- WalterYacuzzi/src/App.jsx
+++ WalterYacuzzi/src/App.jsx
@@ -1,3 +1,10 @@
+// Profe, le dejo mi proyecto...Lo continue sobre la primer entrega, y habia creado otro repositorio en git
+// al final no me dejo guardarlo en ninguno de los nuevos repositorios ni en el antiguo...
+// por eso es tuve que crear este nuevo con todo lo que estaba anteriormente, corregi y entendi lo que me dijo sobre el ICon para
+// que sea reutilizable...Gracias 
+
+
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import Layout from "./components/Layout/Layout";
