cfc648f851fa57e025b839e2892f2279cdb66ba3
MYacuzziWalter <walteryacuzzi@gmail.com>
2024-10-28 00:53:48 -0300

    correccion de itemdetailcontainer para el detalle del producto por firebase

--- WalterYacuzzi/src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ WalterYacuzzi/src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,27 +1,49 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
-import mockProducts from '../../assets/MOCK_DATA.json'
 import { useParams } from 'react-router-dom'
 import ItemDetail from '../ItemDetail/ItemDetail'
+import { doc, getDoc } from "firebase/firestore";
+import { db } from '../../firebase/config'
 
 const ItemDetailContainer = () => {
 
     const [product, setProducts] = useState(null)
-
-    const {id} = useParams()
+    const { id } = useParams()
 
     useEffect(() => {
+        (async () => {
+            try {
+                const docRef = doc(db, "Products", id);
+                const docSnap = await getDoc(docRef);
         
-        const product = mockProducts.find(productToFind => productToFind.id === Number(id))
+                if (docSnap.exists()) {
+                    console.log("Document data:", docSnap.data());
+                    setProducts({...docSnap.data(), id})
+                } else {
+                    // docSnap.data() will be undefined in this case
+                    console.log("No such document!");
+                }
+                
+            } catch (error) {
+                console.log(error);
+                
+            }
 
-        setProducts(product)
-        
-        
+        })()
+
+
+
+
+        // const product = mockProducts.find(productToFind => productToFind.id === Number(id))
+
+        // setProducts(product)
+
+
     }, [id])
 
 
-    return ( product && <ItemDetail product={product}/>
-        
+    return (product && <ItemDetail product={product} />
+
     )
 }
 
